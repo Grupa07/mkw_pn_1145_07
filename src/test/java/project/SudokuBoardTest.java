7230206aff01892c1b0601aa02fba573baf95e8e
229970 Nowacki Patryk <229970@edu.p.lodz.pl>
2020-10-26 08:57:53 +0100

    Dokladniejsze testy

--- src/test/java/project/SudokuBoardTest.java
+++ src/test/java/project/SudokuBoardTest.java
@@ -10,40 +10,44 @@ public class SudokuBoardTest {
         SudokuBoard sudokuBoard = new SudokuBoard();
         sudokuBoard.fillBoard();
 
-        int col = (int) (Math.random() * 9);
-        int row = (int) (Math.random() * 9);
-
-        int x = col - (col % 3); // coordinates of left top cell of the segment
-        int y = row - (row % 3);
-
-        int testVal = sudokuBoard.getBoard(col, row);
-
         // check if there are empty cells (0 represents empty cell)
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
                 Assertions.assertNotEquals(sudokuBoard.getBoard(i, j), 0);
             }
         }
 
         // check if values are repeated in the same column
         for (int i = 0; i < 9; i++) {
-            if (i != row) Assertions.assertNotEquals(sudokuBoard.getBoard(col, i), testVal);
-            else Assertions.assertEquals(sudokuBoard.getBoard(col, i), testVal);
+            for (int j = 0; j < 9; j++) {
+                for (int k = j + 1; k < 9; k++) {
+                    Assertions.assertNotEquals(sudokuBoard.getBoard(i, j), sudokuBoard.getBoard(i, k));
+                }
+            }
         }
-
         // check if values are repeated in the same row
         for (int i = 0; i < 9; i++) {
-            if (i != col) Assertions.assertNotEquals(sudokuBoard.getBoard(i, row), testVal);
-            else Assertions.assertEquals(sudokuBoard.getBoard(i, row), testVal);
+            for (int j = 0; j < 9; j++) {
+                for (int k = j + 1; k < 9; k++) {
+                    Assertions.assertNotEquals(sudokuBoard.getBoard(j, i), sudokuBoard.getBoard(k, i));
+                }
+            }
         }
 
         // Check if values are repeated in the segment
-        for (int i = x; i < x + 3; i++) {
-            for (int j = y; j < y + 3; j++) {
-                if (i != col && j != row) {
-                    Assertions.assertNotEquals(sudokuBoard.getBoard(i, j), testVal);
-                } else if (i == col && j == row) {
-                    Assertions.assertEquals(sudokuBoard.getBoard(i, j), testVal);
+
+        for (int k = 0; k < 81; k++) {
+            int col=k/9;
+            int row=k%9;
+            int x=col-(col%3);
+            int y=row-(row%3);
+            for (int i = x; i < x + 3; i++) {
+                for (int j = y; j < y + 3; j++) {
+                    if (i != col && j != row) {
+                        Assertions.assertNotEquals(sudokuBoard.getBoard(i, j), sudokuBoard.getBoard(col,row));
+                    } else if (i == col && j == row) {
+                        Assertions.assertEquals(sudokuBoard.getBoard(i, j), sudokuBoard.getBoard(col,row));
+                    }
                 }
             }
         }
@@ -59,14 +63,16 @@ public class SudokuBoardTest {
 
         boolean different = false;
 
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (sudokuBoard1.getBoard(i, j) != sudokuBoard2.getBoard(i, j)) {
-                    different = true;
-                }
+        for (int i = 0; i < 81; i++) {
+            int col=i/9;
+            int row=i%9;
+            if (sudokuBoard1.getBoard(i, i) != sudokuBoard2.getBoard(i, i)) {
+                different = true;
+                break;
             }
+
         }
 
-        Assertions.assertEquals(different, true);
+        Assertions.assertTrue(different);
     }
 }
